cd05c050a73f05884cf80defc078f46824cd127f
qvortex-beem <ghujfeuhh1@gmail.com>
2025-09-29 14:51:11 +0300

    добавил принты в Program.cs с сам работ

--- Program.cs
+++ Program.cs
@@ -55,6 +55,34 @@
             //{
             //    Console.WriteLine("Введите корректное число. ");
             //}
+            //Задание 1
+            //Book book = new Book();
+            //book.Print();
+            //Book book2 = new Book("Война и мир", "Толстой", 1869, true);
+            //book2.Print();
+            //Задание 2
+            //BankAccount account = new BankAccount();
+            //account.Deposit(1000);
+            //account.Withdraw(500);
+            //account.PrintBalance();
+            //Задание 3
+            //Car car = new Car("Tesla Model S", 250);
+            //car.Accelerate(120);
+            //Console.WriteLine($"Модель: {car.Model}, Текущая скорость: {car.Speed} км/ч");
+            //car.Brake();
+            //Console.WriteLine($"После торможения: {car.Speed} км/ч");
+            //Задание 4
+            //Dice dice1 = new Dice();
+            //Console.WriteLine($"{dice1._sides}-гранный кубик: {dice1.Roll()},{dice1.Roll()},{dice1.Roll()}");
+            //Dice dice2 = new Dice(12);
+            //Console.WriteLine($"{dice2._sides}-гранный кубик: {dice2.Roll()},{dice2.Roll()},{dice2.Roll()}");
+            //Задание 5
+            //Character воин = new Character(150,50);
+            //Character маг = new Character(50, 250);
+            //Character лучник = new Character(80, 100);
+            //воин.Attack();
+            //маг.Attack();
+            //лучник.Attack();
         }
     }
     
